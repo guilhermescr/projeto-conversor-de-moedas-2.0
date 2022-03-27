7727097cb1c4a0f3488920f4f61f52c0fa3188f6
Guilherme Rocha <97315490+guilhermescr@users.noreply.github.com>
2022-03-27 13:06:35 -0300

    botao reset + melhor responsividade

--- js/js.js
+++ js/js.js
@@ -2,7 +2,9 @@
 let valueEUR = 5.44;
 let valueTemp;
 //selecionando
-let btnConverter = document.querySelector(".botao")
+let boxBotao = document.querySelector(".botao")
+let btnConverter = document.querySelector(".botao1")
+let btnReset = document.querySelector(".botao2")
 let valueSelect1 = document.querySelector(".Valor1") 
 let valueSelect2 = document.querySelector(".Valor2")
 let valueTyped = document.querySelector(".Valor3")
@@ -16,13 +18,32 @@ btnConverter.addEventListener('click', function(event){
     if(valueSelect1.value == "BRL" && valueSelect2.value == "EUR"){
         valueTemp = Number(valueTyped.value)/valueEUR; 
         console.log(`Its value in reais is: ${valueTemp}`)
+        boxBotao.style.margin = '40px -5px 0px -5px'
+        erro.style.display = 'none';
+
     } else if (valueSelect1.value  == "EUR" && valueSelect2.value  == "BRL"){
         valueTemp = Number(valueTyped.value) * valueEUR
         console.log(`Its value in euros is: ${valueTemp}`)
+        boxBotao.style.margin = '40px -5px 0px -5px'
+        erro.style.display = 'none';
+
     }else{
         valueTemp = "Selecione opções diferentes"
+        boxBotao.style.margin = '40px -5px 0px -5px'
+        erro.style.display = 'block'
         erro.innerText = "ERRO: Selecione opções diferentes"
     } 
 
     valueResult.value = valueTemp.toFixed(2)
+})
+
+btnReset.addEventListener('click', function(event){
+    event.preventDefault();
+
+    valueSelect1.value = 'BRL';
+    valueSelect2.value = 'EUR';
+    valueTyped.value = '';
+    valueResult.value = '';
+
+    erro.style.display = 'none';
 })
